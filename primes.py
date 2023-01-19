690790f060d9465b86d631ca82ede1356114368a
João Cabral <joaocabralpv@gmail.com>
2023-01-19 19:10:07 -0100

    Options

--- primes.py
+++ primes.py
@@ -2,7 +2,9 @@ from math import sqrt,ceil
 primes = [2]
 number = 2
 
-while len(primes) < 58:
+Nprimes = int(input("How many primes do you want to calculate? "))
+
+while len(primes) <Nprimes:
     IsPrime = True
     i = 0
     while i < ceil(sqrt(len(primes))) and IsPrime:
@@ -11,5 +13,6 @@ while len(primes) < 58:
         i+=1
     if IsPrime:
         primes.append(number)
-        print(primes)
     number+=1
+
+print("The first {} primes are: ".format(Nprimes) + str(primes).replace("[","").replace("]",""))
